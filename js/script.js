1b3dd3e88e98ae1c06d453192bd9490e3a35218c
thefrontendwebdeveloper <himangshu.web.design@gmail.com>
2022-02-09 13:09:23 +0530

    Nav bar animation completed & also messed up!

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ const ul = document.getElementsByTagName("ul")[0];
 menu.addEventListener("click", () => {
   ul.classList.toggle("active");
 });
+
+const btn = document.getElementById("menu");
+const header = document.querySelector("header");
+
+let isOpen = false;
+
+const toggleNav = () => {
+  if (isOpen) {
+    // * this will run if isOpen == true, ie, if nav is open
+
+    header.style.clipPath = `circle(0vw at calc(100vw - 87.5px) 87.5px)`;
+  } else {
+    // * this will run if isOpen == false, ie, if nav is closed
+
+    header.style.clipPath = `circle(140vw at calc(100vw - 87.5px) 87.5px)`;
+  }
+
+  isOpen = !isOpen;
+};
+
+btn.addEventListener("click", toggleNav);
